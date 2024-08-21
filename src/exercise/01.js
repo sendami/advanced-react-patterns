a2afc2f0396db1636b1e41793fa5a17f5d8a7b63
sendami luque <sendami.luque@thoughtworks.com>
2024-08-21 17:22:58 +0200

    Exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,84 +3,8 @@
 
 import * as React from 'react'
 import {dequal} from 'dequal'
+import {updateUser, UserProvider, useUser} from './01/context/user-context'
 
-// ./context/user-context.js
-
-import * as userClient from '../user-client'
-import {useAuth} from '../auth-context'
-
-const UserContext = React.createContext()
-UserContext.displayName = 'UserContext'
-
-function userReducer(state, action) {
-  switch (action.type) {
-    case 'start update': {
-      return {
-        ...state,
-        user: {...state.user, ...action.updates},
-        status: 'pending',
-        storedUser: state.user,
-      }
-    }
-    case 'finish update': {
-      return {
-        ...state,
-        user: action.updatedUser,
-        status: 'resolved',
-        storedUser: null,
-        error: null,
-      }
-    }
-    case 'fail update': {
-      return {
-        ...state,
-        status: 'rejected',
-        error: action.error,
-        user: state.storedUser,
-        storedUser: null,
-      }
-    }
-    case 'reset': {
-      return {
-        ...state,
-        status: null,
-        error: null,
-      }
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
-    }
-  }
-}
-
-function UserProvider({children}) {
-  const {user} = useAuth()
-  const [state, dispatch] = React.useReducer(userReducer, {
-    status: null,
-    error: null,
-    storedUser: user,
-    user,
-  })
-  const value = [state, dispatch]
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
-
-function useUser() {
-  const context = React.useContext(UserContext)
-  if (context === undefined) {
-    throw new Error(`useUser must be used within a UserProvider`)
-  }
-  return context
-}
-
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
-
-// export {UserProvider, useUser}
-
-// src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -97,12 +21,7 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState)
   }
 
   return (
@@ -162,10 +81,10 @@ function UserSettings() {
           {isPending
             ? '...'
             : isRejected
-            ? '✖ Try again'
-            : isChanged
-            ? 'Submit'
-            : '✔'}
+              ? '✖ Try again'
+              : isChanged
+                ? 'Submit'
+                : '✔'}
         </button>
         {isRejected ? <pre style={{color: 'red'}}>{error.message}</pre> : null}
       </div>
---
+++ src/exercise/01/context/user-context.js
@@ -0,0 +1,77 @@
+import * as userClient from '../../../user-client'
+import {useAuth} from '../../../auth-context'
+import React from 'react'
+
+const UserContext = React.createContext()
+UserContext.displayName = 'UserContext'
+
+function userReducer(state, action) {
+  switch (action.type) {
+    case 'start update': {
+      return {
+        ...state,
+        user: {...state.user, ...action.updates},
+        status: 'pending',
+        storedUser: state.user,
+      }
+    }
+    case 'finish update': {
+      return {
+        ...state,
+        user: action.updatedUser,
+        status: 'resolved',
+        storedUser: null,
+        error: null,
+      }
+    }
+    case 'fail update': {
+      return {
+        ...state,
+        status: 'rejected',
+        error: action.error,
+        user: state.storedUser,
+        storedUser: null,
+      }
+    }
+    case 'reset': {
+      return {
+        ...state,
+        status: null,
+        error: null,
+      }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
+
+function UserProvider({children}) {
+  const {user} = useAuth()
+  const [state, dispatch] = React.useReducer(userReducer, {
+    status: null,
+    error: null,
+    storedUser: user,
+    user,
+  })
+  const value = [state, dispatch]
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
+
+function useUser() {
+  const context = React.useContext(UserContext)
+  if (context === undefined) {
+    throw new Error(`useUser must be used within a UserProvider`)
+  }
+  return context
+}
+
+function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates: updates})
+  userClient.updateUser(user, updates).then(
+    updatedUser => dispatch({type: 'finish update', updatedUser}),
+    error => dispatch({type: 'fail update', error}),
+  )
+}
+
+export {UserProvider, useUser, updateUser}
